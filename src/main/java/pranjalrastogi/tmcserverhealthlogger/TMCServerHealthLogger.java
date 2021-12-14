41e49bc6d8fc84c0170fa10f8b15b7496f52a605
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2021-12-14 23:19:32 +0530

    add basic

--- src/main/java/pranjalrastogi/tmcserverhealthlogger/TMCServerHealthLogger.java
+++ src/main/java/pranjalrastogi/tmcserverhealthlogger/TMCServerHealthLogger.java
@@ -1,4 +1,4 @@
-package pranjalrastogi.ds8statstracker;
+package pranjalrastogi.tmcserverhealthlogger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,11 +7,12 @@ public final class TMCServerHealthLogger extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getLogger().info("[TMC ServerHealthLogger] Plugin enabled.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        this.getLogger().info("[TMC ServerHealthLogger] Plugin disabled.");
     }
 }
